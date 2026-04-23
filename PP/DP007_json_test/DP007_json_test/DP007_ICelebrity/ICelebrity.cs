e7b0c3cc72f33369a85009178528790af3e79b68
Keaichi56 <fedor19uyfd@gmail.com>
2026-04-23 18:34:58 +0300

    new labs

--- PP/DP007_json_test/DP007_json_test/DP007_ICelebrity/ICelebrity.cs
+++ PP/DP007_json_test/DP007_json_test/DP007_ICelebrity/ICelebrity.cs
@@ -3,15 +3,15 @@ using System.Collections.Generic;
 
 namespace DP007_ICelebrity
 {
-    public interface ICelebrity<T> : IDisposable // интерфейс для работы с знаменитостями
+    public interface ICelebrity<T> : IDisposable 
     {
-        List<T> GetAllCelebrities(); // получаем всех знаменитостей
-        T GetCelebrityById(int id); // получаем знаменитость по ID
-        bool DelCelebrity(int id); // удаляем знаменитость по ID
-        bool AddCelebrity(T celebrity); // добавляем знаменитость
-        int AddCelebrityAndGetId(T celebrity); // добавляем знаменитость и получаем её ID
-        bool UpdCelebrity(int id, T celebrity); // обновляем знаменитость по ID
-        int GetCelebrityIdByName(string name); // получаем ID знаменитости по имени
-        int SaveChanges(); // сохраняем изменения
+        List<T> GetAllCelebrities(); 
+        T GetCelebrityById(int id);
+        bool DelCelebrity(int id);
+        bool AddCelebrity(T celebrity); 
+        int AddCelebrityAndGetId(T celebrity); 
+        bool UpdCelebrity(int id, T celebrity); 
+        int GetCelebrityIdByName(string name); 
+        int SaveChanges();
     }
 }
--- ВЯП/lab6/lab06.py
+++ ВЯП/lab6/lab06.py
@@ -2,24 +2,4 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 
-# Шаг 1: Получаем HTML-страницу
-url = 'http://books.toscrape.com/'
-response = requests.get(url)
-soup = BeautifulSoup(response.text, 'lxml')
-
-# Шаг 2: Ищем все карточки с товарами
-books = soup.find_all('article', class_='product_pod')
-
-data = []
-for book in books:
-    title = book.h3.a['title']
-    price_str = book.find('p', class_='price_color').text
-    # Очищаем цену от символа фунта и преобразуем в число
-    price = float(price_str.replace('Â£', ''))
-    data.append([f"{title}, \t{price_str}"])
-
-# Шаг 3: Сохраняем в CSV
-with open('books.csv', 'w', newline='', encoding='utf-8') as file:
-    writer = csv.writer(file, delimiter=';')
-    writer.writerow(['Title', '\t\t\t\t\tPrice'])
-    writer.writerows(data)
+from 
