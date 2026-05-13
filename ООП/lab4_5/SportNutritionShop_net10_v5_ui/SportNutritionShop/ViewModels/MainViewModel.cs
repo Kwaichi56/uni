db83dc29c06fdb428f91420ac0c8606591a126a2
Kwaichi56 <fedor19uyfd@gmail.com>
2026-05-13 11:59:42 +0300

    oop 5 edit

--- ООП/lab4_5/SportNutritionShop_net10_v5_ui/SportNutritionShop/ViewModels/MainViewModel.cs
+++ ООП/lab4_5/SportNutritionShop_net10_v5_ui/SportNutritionShop/ViewModels/MainViewModel.cs
@@ -2,6 +2,8 @@ using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Globalization;
 using System.Linq;
+using System;
+using System.IO;
 using System.Windows;
 using System.Windows.Data;
 using System.Windows.Input;
